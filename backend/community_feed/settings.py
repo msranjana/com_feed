ecf8245104fb7a3ebcbefbcea915912991766c29
msranjana <msranjana22@gmail.com>
2026-02-02 11:21:02 +0530

    update start.sh

--- backend/community_feed/settings.py
+++ backend/community_feed/settings.py
@@ -147,6 +147,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
+    "https://com-feed.onrender.com/",
+    
 ]
 
 CORS_ALLOW_CREDENTIALS = True
